ef013fb2fd4b7ed2243962883c708ae536fba19c
CristianChinchilla <cchinchillafonseca@gmail.com>
2024-11-17 01:35:24 -0600

    Añadi un pequeño booleano que indica si el usuario es administrador o no

--- ReservationSystem/src/reservationsystem/User.java
+++ ReservationSystem/src/reservationsystem/User.java
@@ -1,4 +1,5 @@
 package reservationsystem;
+import java.util.Scanner;
 
 public class User {
 
@@ -12,7 +13,32 @@ public class User {
 
     private NotificationType preferredType;
 
+    private boolean IsAdmin;
+
+    Scanner sc = new Scanner(System.in);
+
+
+
     public void selectTypeUser() {
+        System.out.println("""
+                Escoga el tipo de usuario:
+                1. Cliente
+                2. Administrador
+                """);
+        String selection = sc.next();
+
+        switch (selection) {
+            case "1" -> {
+                IsAdmin = true;
+            }
+            case "2" -> {
+                IsAdmin = false;
+            }
+            default -> {
+                System.out.println("Opción no valida");
+                selectTypeUser();
+            }
+        }
     }
 
     public void chooseNotificationType() {
